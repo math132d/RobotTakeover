6b6c854e6860a4f756f26605e2a164423c68d0c8
mjha17 <36537048+mjha17@users.noreply.github.com>
2019-08-21 20:20:01 +0200

    movement stuff completish

--- Entities/Character.gd
+++ Entities/Character.gd
@@ -3,32 +3,73 @@ extends KinematicBody2D
 enum {UP, DOWN, LEFT, RIGHT}
 
 #Export all "default values" not to be modified during runtime
-export (int) var DEFAULT_SPEED = 20
+export (int) 	var DEFAULT_ACCL_SPEED = 60   #Pixels added to movement vec per second
+export (float) 	var DEFAULT_FRICTION   = 20  #Pixels subtracted from movement vec per second
+export (int) 	var DEFAULT_MAX_SPEED  = 60  #Max speed in pixels per second
+
 export (int) var DEFAULT_HEALTH = 100
-export (float) var DEFAULT_FRICTION = 2
 export (PackedScene) var active_weapon
+
+#Statemachine used to control animation
+onready var statemachine = $Character/AnimationTree.get("parameters/playback")
 
 #Apply default values to modifyable vars
 onready var health = DEFAULT_HEALTH
-onready var speed = DEFAULT_SPEED
+onready var max_speed = DEFAULT_MAX_SPEED
+onready var accl_speed = DEFAULT_ACCL_SPEED
 
 var facing = DOWN
 
-var accl_vec = Vector2(0,0)
 var move_vec = Vector2(0,0)
+var accl_vec = Vector2(0,0)
 
-func _process(delta):
+func _physics_process(delta):
 	
-	move_vec += accl_vec
-	accl_vec *= 0
-	
+	#This code adds acceleration to movement in a linear fashion
+	#Ensures that movement vectors magnitude will never be higher then max speed.
+	if move_vec.length() < max_speed:
+		var delta_to_max = max_speed - move_vec.length()
+		move_vec += accl_vec.normalized() * min((accl_speed * delta), delta_to_max)
+		
 	move_and_slide(move_vec)
 	
-	move_vec -= move_vec.normalized()*DEFAULT_FRICTION
-	
-	if move_vec.length() < 2:
-		move_vec = Vector2(0,0)
+	#This code subtracts friction from movement in a linear fashion
+	#Ensures that friction will never cause "negative" movement in case friction is heigher then delta to min
+	if move_vec.length() > 0:
+		var delta_to_min = move_vec.length()
+		move_vec -= move_vec.normalized() * min((DEFAULT_FRICTION * delta), delta_to_min)
 		
+	accl_vec *= 0
+	
+	#Flip the sprite if facing right
+	if facing == RIGHT:
+		$Character.set_flip_h(true)
+	else:
+		$Character.set_flip_h(false)
+	
+	#Travels to the appropriate animation in the statemachine
+	if move_vec.length() <= (DEFAULT_FRICTION*delta):
+		#Considers the character stationary
+
+		match facing:
+			UP:
+				statemachine.travel("IdleUp")
+			DOWN:
+				statemachine.travel("IdleDown")
+			LEFT, RIGHT:
+				statemachine.travel("IdleSide")
+	else:
+		#The character is walking. 
+		match facing:
+			UP:
+				statemachine.travel("WalkUp")
+			DOWN:
+				statemachine.travel("WalkDown")
+			LEFT, RIGHT:
+				statemachine.travel("WalkSide")
+
+	pass #END OF PHYSICS PROCESS
+
 func _on_BulletDetecetor_entered(body):
 	bullet_hit(body)
 	
@@ -38,7 +79,9 @@ func direction_to_facing(vector:Vector2):
 	
 	var facing = DOWN
 	
-	if abs(vector.x) > abs(vector.y):
+	#In case of diagonal movement we would rather have the side view then face-on
+	#Therefore the x-axis is weighed higher
+	if abs(vector.x * 2) > abs(vector.y):
 		facing = LEFT if vector.x < 0 else RIGHT
 	else:
 		facing = UP if vector.y < 0 else DOWN
@@ -47,21 +90,9 @@ func direction_to_facing(vector:Vector2):
 
 func move(direction):
 	
-	direction = direction.normalized()
-	accl_vec = direction*speed
+	accl_vec = direction.normalized()
 	
-	self.facing = direction_to_facing(direction)
-	
-	#Plays the appropriate animation
-	match self.facing:
-		UP:
-			$Character/AnimationPlayer.play("WalkUp")
-		DOWN:
-			$Character/AnimationPlayer.play("WalkDown")
-		LEFT:
-			$Character/AnimationPlayer.play("WalkLeft")
-		RIGHT:
-			$Character/AnimationPlayer.play("WalkRight")
+	self.facing = direction_to_facing(move_vec.normalized())
 	
 	pass
 	
@@ -72,11 +103,14 @@ func bullet_hit(body):
 	
 	pass
 	
-#func fire(direction):
-#	active_weapon.fire(direction)
-
-#DEBUG CRAP
-func _input(event):
-	if event is InputEventMouseButton:
-		self.move_vec *= 0
-		move((event.position-self.position).normalized())
+func attack(direction):
+	
+	self.facing = direction_to_facing(direction.normalized())
+	
+	match facing:
+		UP:
+			statemachine.start("AttackUp")
+		DOWN:
+			statemachine.start("AttackDown")
+		LEFT, RIGHT:
+			statemachine.start("AttackSide")
